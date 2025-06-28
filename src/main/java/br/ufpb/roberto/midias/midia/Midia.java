ea0a4141b0d194179c56e1d3011fb435888452b5
RobertoJunior <roberto.lima1236@gmail.com>
2025-06-28 14:04:52 -0300

    atualizando

--- src/main/java/br/ufpb/roberto/midias/midia/Midia.java
+++ src/main/java/br/ufpb/roberto/midias/midia/Midia.java
@@ -1,4 +1,19 @@
 package br.ufpb.roberto.midias.midia;
 
 public class Midia {
+    private String titulo;
+    private String genero;
+    private int anoLancamento;
+    private String descricao;
+    private String [] elenco;
+    private String diretor;
+
+    public Midia(String titulo, String genero, int anoLancamento, String descricao, String [] elenco, String diretor){
+        this.titulo = titulo;
+        this.genero = genero;
+        this.anoLancamento = anoLancamento;
+        this.descricao = descricao;
+        this.elenco = elenco;
+        this.diretor = diretor;
+    }
 }
